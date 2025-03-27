d6c2edf816c0723f908e17bc9c561d8ac208820b
LucasBb07 <barcelloslucas07@gmail.com>
2025-03-27 16:33:53 -0300

    aula_dawn

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.set('view engine', 'ejs')
 import {fileURLToPath} from 'url';
 import {dirname} from 'path';
 import routes from "./routes/route.js"
+import exp from 'constants';
+import mongoose, { Mongoose } from 'mongoose';
+const url = "mongodb+srv://aluno:aluno@cluster0.kgvfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(url)
+console.log(mongoose.connect)
+
+
 
 //Converte o caminho do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
